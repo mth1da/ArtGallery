a6b66a3bf2e6bcc1c7fe4c40806c05e4956c4567
mth1da <113670955+mth1da@users.noreply.github.com>
2023-03-13 21:22:00 +0100

    fixing db

--- src/main/java/Data.java
+++ src/main/java/Data.java
@@ -3,11 +3,13 @@ import java.sql.*;
 public class Data {
 
     public static void main(String[] arg) throws SQLException {
-        Connection con= DriverManager.getConnection("jdbc:mariadb://localhost:3308/ArtGallery","root","meryam");
-        Statement statement=con.createStatement();
-        ResultSet re=statement.executeQuery("Select * from visiteur");
-        while(re.next()){
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/artgallery", "root", "");
+        Statement statement = con.createStatement();
+        ResultSet re = statement.executeQuery("Select * from visiteur");
+        ResultSet res = statement.executeQuery("Insert into reservation (idReservation) values (5)");
+        while (re.next()) {
             System.out.println(re.getString("name"));
+            System.out.println(res.getString("idReservation"));
         }
 
     }
